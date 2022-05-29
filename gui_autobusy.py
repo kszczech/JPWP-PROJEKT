2c68cd6b1c4ebe2da379b4dbc88a4a59480e2a17
tomekmaz723 <72949690+tomekmaz723@users.noreply.github.com>
2022-05-29 22:14:44 +0200

    Update gui_autobusy.py

--- gui_autobusy.py
+++ gui_autobusy.py
@@ -4,7 +4,6 @@ from Autobusy_main import *
 from Bus import Bus
 
 
-
 def input_funkcja(root):
     root = root
     root.filename = filedialog.askopenfilename(initialdir="c:/pdw",
@@ -25,54 +24,37 @@ def output_funkcja(root,buses):
     zapisz_do_arkusza(buses, root.filename)
 
 
-
-def clicked(r):
+def clicked_but(colname):
     global col
-    if r == 1:
-        col = 'Zabawometr'
-    else:
-        col = 'Kierunek'
-    return col
-
-def clicked_but(dymy):
-    print(dymy)
-    global col
-    if dymy == 1:
-        col = 'Zabawometr'
-    elif dymy == 2:
-        col = 'Kierunek'
+    col = colname
+    print(colname)
     return col
 
 def display():
 
     root = Tk()
     root.title("Przydzielanie osób do autobusów")
-    #root.geometry('400x400')
 
-    global col
-    x = 30
-    y = x - 1
-    z = x - 2
-    q = x - 0
-    b1 = Bus("b1", x, y)
-    b2 = Bus("b2", x, z)
-    b3 = Bus("b3", x, q)
-    buses = [b1, b2, b3]
+
+    # stworz autobusy x => liczba miejsc
+    x = 50
+    # b1 = Bus("nazwa_autobusu", [wszystkie_miejsca], [wolne_miejsca])
+    # tworzymy ile chcemy
+    b1 = Bus("b1", x, x)
+    b2 = Bus("b2", x, x)
+    b3 = Bus("b3", x, x)
+    b4 = Bus("b4", x, x)
+    b5 = Bus("b5", x, x)
+    # dodajemy do tablicy buses po przecinku
+    buses = [b1, b2, b3, b4, b5]
     global wb
-
-
-    r = IntVar()
-    r.set(1)
-
 
 
 
     input = Button(root, text="Input file", padx= 60, pady=30, command=lambda : input_funkcja(root))
     output = Button(root, text="Output file", padx= 60, pady=30, command=lambda :output_funkcja(root,buses))
-    zabawa = Radiobutton(root, text="Zabawometr", variable=r, value=1, command=lambda: clicked_but(r.get()))
-    kierunki = Radiobutton(root, text="Kierunek", variable=r,  value=2,command=lambda: clicked_but(r.get()))
-
-
+    zabawa = Radiobutton(root, text="Zabawometr",  value=1, command=lambda: clicked_but('Zabawometr'))
+    kierunki = Radiobutton(root, text="Kierunek",   value=2,command=lambda: clicked_but('Kierunek'))
     input.grid(row=1, column=0)
     output.grid(row=1, column=1)
     zabawa.grid(row=0, column=0)
